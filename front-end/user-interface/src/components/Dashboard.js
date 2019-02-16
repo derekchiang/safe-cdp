97bd0c1acb90b069a243638efd7c04227fd2b178
Noah Amar <ne.amar@outlook.com>
2019-02-16 16:54:08 -0700

    dashboard layout

--- front-end/user-interface/src/components/Dashboard.js
+++ front-end/user-interface/src/components/Dashboard.js
@@ -1,13 +1,17 @@
 import React, { Component, Fragment } from "react";
 
-
 //components
 import SideBar from "./Sidebar";
 import CollateralRatio from "./CollateralRatio";
+import DebtCard from "./DebtCard";
+import CollateralCard from "./CollateralCard";
+import IDCard from "./IDCard";
+import FeesCard from "./FeesCard";
 
 //css, images
 import "../CSS/Dashboard.css";
-
+import LiquidCard from "./LiquidCard";
+import EthereumCard from "./EthereumCard";
 
 class Dashboard extends Component {
   constructor(props) {
@@ -18,9 +22,27 @@ class Dashboard extends Component {
     return (
       <div className="dashboard">
         <Fragment>
-          <SideBar className="clientNavSidebar"/>
+          <SideBar className="clientNavSidebar" />
           <div className="dashboardContainer">
-          <CollateralRatio />
+            <CollateralRatio className="cardDivTop" />
+            <div className="cardTopLeft">
+              <DebtCard />
+            </div>
+            <div className="cardTopRight">
+              <CollateralCard />
+            </div>
+            <div className="cardBottom">
+              <IDCard />
+            </div>
+            <div className="cardBottom2">
+                <FeesCard />
+            </div>
+            <div className="cardBottom3">
+                <LiquidCard />
+            </div>
+            <div className="cardBottom4">
+                <EthereumCard />
+            </div>
           </div>
         </Fragment>
       </div>
---
+++ front-end/user-interface/src/components/EthereumCard.js
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+
+const styles = {
+  card: {
+    minWidth: 281,
+    minHeight: 106,
+    marginTop: 50
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: 600,
+    color: "#95aac9",
+    fontFamily: "Cerebri Sans, sans-serif",
+    paddingTop: 10
+  },
+  pos: {
+    marginBottom: 12
+  }
+};
+
+function SimplestCard(props) {
+  const { classes } = props;
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Ethereum Price
+        </Typography>
+        
+      </CardContent>
+      {/* <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions> */}
+    </Card>
+  );
+}
+
+SimplestCard.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(SimplestCard);
