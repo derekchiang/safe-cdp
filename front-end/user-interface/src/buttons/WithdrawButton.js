855a463726877c39d95c51127eca413f8e80168b
Noah Amar <ne.amar@outlook.com>
2019-02-16 23:12:02 -0700

    Deposit and Withdraw buttons

---
+++ front-end/user-interface/src/buttons/WithdrawButton.js
@@ -0,0 +1,32 @@
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import NavigationIcon from '@material-ui/icons/Navigation';
+
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+});
+
+function ButtonSizes(props) {
+  const { classes } = props;
+  return (
+      <div>
+        <Button variant="contained" size="medium" color="primary" className={classes.margin}>
+          Withdraw Funds
+        </Button>
+      </div>
+  );
+}
+
+ButtonSizes.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonSizes);
