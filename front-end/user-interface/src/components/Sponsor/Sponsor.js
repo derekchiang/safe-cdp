076cc8d87554d7700abc90cb650676444fcecb99
Brandon West <BrandonWest@Brandons-MacBook-Pro-3.local>
2019-02-17 04:25:46 -0700

    add to sponsor page

--- front-end/user-interface/src/components/Sponsor/Sponsor.js
+++ front-end/user-interface/src/components/Sponsor/Sponsor.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import SideBar from '../Sidebar';
 import WithdrawModal from './WithdrawModal';
 import DepositModal from './DepositModal';
-
+import SponsorCard from './SponsorCard.js';
 
 class Sponsor extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
-    render() { 
-        return ( 
+    render() {
+        return (
             <div>
                 <SideBar />
+                <SponsorCard />
                 <div>
                 <WithdrawModal />
                 </div>
@@ -23,5 +24,5 @@ class Sponsor extends Component {
          );
     }
 }
- 
+
 export default Sponsor;
